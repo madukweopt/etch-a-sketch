7a18e5acd95db2e9e8174a239ed668b8762ae8ba
pascal <madukwepascal@gmail.com>
2022-07-05 21:04:55 +0100

    Create and append child

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+
+function createGrid (gridNum) {
+    for (let i = 0; i < gridNum; i++) {
+        const rows = document.createElement('div');
+        rows.classList.add('grid-row');
+
+        for (let j = 0; j < gridNum; j++) {
+        const cells = document.createElement('div');
+        cells.classList.add('cell'); 
+        let heightAndWidth = 600/gridNum;
+        cells.style.width = `${heightAndWidth}px`;
+        cells.style.height = `${heightAndWidth}px`;
+        rows.appendChild(cells); 
+    }
+    
+    container.appendChild(rows);
+}
+
+}
+createGrid(16);
