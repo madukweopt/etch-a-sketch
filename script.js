75b79ebfc80fa4418253f66b2461c976cd99b43b
pascal <madukwepascal@gmail.com>
2022-07-07 10:21:32 +0100

    Modify UI and add reset button feature

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const resetGrid = document.querySelector('.reset-btn');
 
 function createGrid (gridNum) {
     for (let i = 0; i < gridNum; i++) {
@@ -8,7 +9,7 @@ function createGrid (gridNum) {
         for (let j = 0; j < gridNum; j++) {
         const cells = document.createElement('div');
         cells.classList.add('cell'); 
-        let heightAndWidth = 600/gridNum;
+        let heightAndWidth = 500/gridNum;
         cells.style.width = `${heightAndWidth}px`;
         cells.style.height = `${heightAndWidth}px`;
         rows.appendChild(cells); 
@@ -19,29 +20,43 @@ function createGrid (gridNum) {
 
 }
 createGrid(16);
+changeColor();
 
+function changeColor() {
 const allCells = document.querySelectorAll('.cell');
     for (let grid of allCells) {
         grid.addEventListener('mouseenter', function() {
             grid.style.backgroundColor = 'black';
+            
         })
 
     }
-    const btn = document.querySelector('.btn');
+}
+    const btn = document.querySelector('.change-btn');
     btn.addEventListener('click', function() {
-        let gridNum = Number(prompt('enter number of grids to display'))
+        let gridNum = Number(prompt('enter number of grids to display: less than 100'))
         if (gridNum > 100){
         alert('enter number less than 100');
-        return;
 
         } else if (gridNum <= 100) {
         clearGrid();
         createGrid(gridNum);
+        changeColor()
         }
     })
 
     function clearGrid() {
         container.innerHTML = '';
     }
+
+resetGrid.addEventListener('click', formatGrid);
+
+    function formatGrid() {
+    const allCells = document.querySelectorAll('.cell');
+    for (let grid of allCells) {
+        grid.style.backgroundColor = '#faf9f6';
+        
+  }
+ }  
 
 
